61492a8632cec4adf12260c7ba7980094c123707
JuliaTurych <julia.turich@gmail.com>
2022-10-13 21:13:15 -0500

    update a first class

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,5 +3,11 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        //this a new line
+
+        // i want to create a history
+
+        // the text doesn't matter
     }
 }
