9ed6a6be543cff5020228a819fd7cd4b872c094c
JuliaTurych <julia.turich@gmail.com>
2022-10-13 21:58:39 -0500

    create update class

--- src/Third.java
+++ src/Third.java
@@ -1,5 +1,11 @@
 public class Third {
+    String brand;
+    public Third(String brand){
+        this.brand = brand;
+    }
     public static void Car(String brand){
         System.out.println("The brand of Car is "+ brand);
     }
+
+
 }
