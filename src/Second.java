7cdf3492f45489acf6b3b6b955e960eedef953e9
JuliaTurych <julia.turich@gmail.com>
2022-10-13 21:45:21 -0500

    updated second file

--- src/Second.java
+++ src/Second.java
@@ -1,13 +1,13 @@
 public class Second {
     public void m (){
-
+        System.out.println("First method");
     }
 
     public void n (int a){
-
+        System.out.println("Second method");
     }
 
     public void n (String s){
-
+        System.out.println("Third method");
     }
 }
